af8919b3d7abfc8d290887bfc60aa8cb92c2a212
ppenter <jarupak.sri@gmail.com>
2024-02-07 01:52:36 +0700

    🔥 refactor(route.ts): remove unused imports and commented out code to improve code readability and maintainability

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,8 +2,6 @@ import { kv } from "@vercel/kv";
 import { Ratelimit } from "@upstash/ratelimit";
 import { OpenAI } from "openai";
 import { OpenAIStream, OpenAIStreamCallbacks, StreamingTextResponse } from "ai";
-// import * as FunctionApp from "./functions";
-// import { TFunctionApp } from "./functions";
 import { getToken } from "next-auth/jwt";
 import { encodingForModel } from "js-tiktoken";
 import { calTokenFromContext } from "@/lib/utils";
@@ -15,70 +13,6 @@ import { prisma } from "@/lib/prisma";
 // });
 
 export const runtime = "edge";
-
-// const call_function = async (
-//   plugin: string,
-//   fname: string,
-//   args: any,
-//   options?: {
-//     authorization?: string;
-//     host?: string;
-//   },
-// ) => {
-//   try {
-//     const base_url =
-//       process.env.NODE_ENV === "development"
-//         ? "http://localhost:3000"
-//         : "https://luim-chat.vercel.app";
-
-//     const _manifest = await fetch(`${base_url}/plugins/${plugin}.json`);
-//     const manifest = await _manifest.json();
-
-//     const api = manifest?.api?.find((api: any) => api.name === fname);
-
-//     let hostname = manifest.hostname || options?.host;
-//     let method = api?.method || "GET";
-//     if (hostname[hostname.length - 1] === "/") {
-//       hostname = hostname.slice(0, hostname.length - 1);
-//     }
-//     hostname = hostname + api?.url;
-//     Object.keys(api?.parameters?.properties)?.map((prop: any) => {
-//       const property = api?.parameters?.properties?.[prop];
-//       if (property?.dynamicPath) {
-//         hostname = hostname + args[prop];
-//         delete args[prop];
-//       }
-//     });
-
-//     if (method === "GET") {
-//       let params = "?";
-//       Object.keys(args).map((key) => {
-//         params = params + key + "=" + JSON.stringify(args[key]) + "&";
-//       });
-//       hostname = `${hostname}${params}`;
-//     }
-
-//     const res = await fetch(`${hostname}`, {
-//       method: api?.method || "GET",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: options?.authorization ? options?.authorization : "",
-//       },
-//       // body: JSON.parse(message?.function_call?.arguments)
-//     });
-//     if (!res.ok) {
-//       return res.statusText;
-//     } else {
-//       // set last message to assistant and res json
-//       const json = await res.json();
-//       return json;
-//       // edit last message
-//       // setMessages(messages)
-//     }
-//   } catch (err: any) {
-//     return err?.message;
-//   }
-// };
 
 export async function POST(req: Request) {
   try {
@@ -243,29 +177,6 @@ export async function POST(req: Request) {
     const stream = OpenAIStream(
       initialResponse,
       streamCallbacks,
-      //   , {
-      //   experimental_onFunctionCall: async (
-      //     { name, arguments: args }: any,
-      //     createFunctionCallMessages: any,
-      //   ) => {
-      //     const plugin_name = name.split("___")[0];
-      //     const api_name = name.split("___")[1];
-      //     const result = await call_function(plugin_name, api_name, args, {
-      //       authorization: plugins?.find(
-      //         (plugin: any) => plugin.name === plugin_name,
-      //       )?.authorization,
-      //       host: plugins?.find((plugin: any) => plugin.name === plugin_name)
-      //         ?.url,
-      //     });
-      //     const newMessages = createFunctionCallMessages(result);
-      //     return openai.chat.completions.create({
-      //       model: model,
-      //       stream: true,
-      //       messages: [...messages, ...newMessages],
-      //       max_tokens,
-      //     });
-      //   },
-      // } as any
     );
 
     return new StreamingTextResponse(stream, undefined);
